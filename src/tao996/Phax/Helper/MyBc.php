485264deb368ede92c10c680591dddc9cfe5708e
tao996 <lvshutao@outlook.com>
2026-07-09 10:34:52 +0800

    MyBc 比较时添加了差值

--- src/tao996/Phax/Helper/MyBc.php
+++ src/tao996/Phax/Helper/MyBc.php
@@ -36,7 +36,7 @@ class MyBc
      */
     public static function divWith(array &$data, $key, $value, int $scale = 2): void
     {
-        if ($value == 0){
+        if ($value == 0) {
             throw new \Exception('除数不能为0');
         }
         $v = $data[$key] ?? 0;
@@ -66,7 +66,7 @@ class MyBc
 
     public static function div($d1, $d2, int $scale = 2): string
     {
-        if ($d2 == 0){
+        if ($d2 == 0) {
             throw new \Exception('除数不能为0');
         }
         return bcdiv('' . $d1, '' . $d2, $scale);
@@ -77,7 +77,7 @@ class MyBc
      * @param $d2
      * @return int 0: 相等， 1: d1>d2； -1: d1<d2
      */
-    public static function cmp($d1, $d2):int
+    public static function cmp($d1, $d2): int
     {
         return bccomp('' . $d1, '' . $d2);
     }
@@ -88,13 +88,22 @@ class MyBc
      * @param $d2
      * @return bool
      */
-    public static function equal($d1, $d2):bool
+    public static function equal($d1, $d2): bool
     {
         return bccomp('' . $d1, '' . $d2) == 0;
     }
 
-    public static function notEqual($d1, $d2):bool
+    /**
+     * @param $d1
+     * @param $d2
+     * @param float $diffValue 差值，在差值内，可视为相等
+     * @return bool
+     */
+    public static function notEqual($d1, $d2, float $diffValue = 0): bool
     {
+        if ($diffValue != 0) {
+            return abs(bcsub('' . $d1, '' . $d2)) > $diffValue;
+        }
         return bccomp('' . $d1, '' . $d2) != 0;
     }
 }
